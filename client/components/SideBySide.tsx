af2013835720fe59a29b3df056427030ee1cc559
Lewis <lewrodriguez18@saintpeters.edu>
2021-09-15 12:41:55 -0400

    Update SideBySide.tsx

--- client/components/SideBySide.tsx
+++ client/components/SideBySide.tsx
@@ -1,21 +1,26 @@
-import { Flex, Image, Heading, Divider } from "@chakra-ui/react";
+import { Flex, Image, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 
 interface Props {}
 
 const SideBySide = (props: Props) => {
+  const [isLargerThan] = useMediaQuery("(min-width: 500px)");
   return (
     <>
-      <Heading padding="20px" textAlign="center" bg="gray.800" color="white">
+      <Heading padding="40px" textAlign="center" bg="gray.800" color="white">
         Our Services
       </Heading>
       <Flex flexDirection="column" bg="gray.800" color="white" padding="20px">
-        <Flex justifyContent="space-evenly">
-          <Flex w="25%">
+        <Flex justifyContent="space-evenly" flexDirection={isLargerThan ? "row" : "column"}>
+          <Flex flexDirection="column">
             <Heading>Airport Transporation</Heading>
+            <Text color="gray.300">dsfsdfsdfsf</Text>
           </Flex>
           <Image
             borderRadius="10px"
             src="artur-tumasjan-KZSNMN4VxR8-unsplash.jpg"
+            bgPos="center"
+            bgSize="cover"
+            alt="Image"
           />
         </Flex>
         <Flex>
