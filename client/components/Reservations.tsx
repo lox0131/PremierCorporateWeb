c302a668c6766ffa2d76f23cfe4ed223aeb83b7d
Lewis <lewrodriguez18@saintpeters.edu>
2021-09-21 14:13:29 -0400

    added date and other things

--- client/components/Reservations.tsx
+++ client/components/Reservations.tsx
@@ -3,8 +3,7 @@ import {
   Heading,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
+  DarkMode,
   Input,
   useMediaQuery,
 } from "@chakra-ui/react";
@@ -28,32 +27,67 @@ const Reservations: React.FC = () => {
         bg="gray.800"
         color="white"
         padding="40px"
+        paddingBottom={20}
       >
-        Reversation Form
+        Reservation Form
       </Heading>
       <Flex
         w="100%"
-        h="90vh"
+        minH="75vh"
         bg="gray.800"
         color="white"
         justifyContent="center"
       >
         <Flex
-          w="60%"
+          w="90%"
           minW="325px"
+          maxW="1400px"
+          padding="20px"
           h="70%"
           borderWidth="1px"
-          boxShadow="2xl"
+          boxShadow="dark-lg"
           borderRadius="20px"
           flexDirection={isMinWidthMedium ? "row" : "column"}
         >
-          <Flex w={isMinWidthMedium ? "50%" : "100%"}>
+          <Flex w={isMinWidthMedium ? "50%" : "100%"} flexDirection="column">
+            <FormControl id="email" padding="20px">
+              <FormLabel>Email address</FormLabel>
+              <Input type="date" color="white" />
+            </FormControl>
+            <FormControl id="date" padding="20px">
+              <FormLabel>date address</FormLabel>
+              <Input type="date" />
+            </FormControl>
+            <FormControl id="email" padding="20px">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="email" padding="20px">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
             <FormControl id="email" padding="20px">
               <FormLabel>Email address</FormLabel>
               <Input type="email" />
             </FormControl>
           </Flex>
-          <Flex w={isMinWidthMedium ? "50%" : "100%"}>
+          <Flex w={isMinWidthMedium ? "50%" : "100%"} flexDirection="column">
+            <FormControl id="email" padding="20px">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="email" padding="20px">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="email" padding="20px">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="email" padding="20px">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
             <FormControl id="email" padding="20px">
               <FormLabel>Email address</FormLabel>
               <Input type="email" />
